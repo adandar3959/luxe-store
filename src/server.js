afe07d5e469081468cc141c8b64723c2f86139f2
75503959 <adandar3959@gmail.com>
2025-12-25 00:41:54 +0500

    Update port configuration for deployment

--- src/server.js
+++ src/server.js
@@ -44,7 +44,8 @@ app.get(/(.*)/, (req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
